ec2c027e5bdac987fbb3396a8443333914621d39
Rishabh Umredkar <umredkarrishabh@gmail.com>
2025-03-10 16:27:23 +0530

    mpping

--- Spring_Boot_1/src/main/java/com/qsp/Spring_Boot_1/Controller.java
+++ Spring_Boot_1/src/main/java/com/qsp/Spring_Boot_1/Controller.java
@@ -1,13 +1,18 @@
 package com.qsp.Spring_Boot_1;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/qsp/deccan/student")
 public class Controller {
 	
 	@GetMapping("/hi")
@@ -54,16 +59,46 @@ public class Controller {
 		return "Id : "+id+" Name : "+name;
 	}
 	
+	//----------------------------------------------------------------
 
+	@GetMapping("/RequesrJSON")
+	public Student getStrudnet(@RequestBody Student student)
+	{
+		return student;
+	}
+	
+	
+	//----------------------------------------------------------------
+	@GetMapping("/findByID")
+	public Employee findBYIDmoloyee(@RequestBody List<Employee> list)
+	{
+		for(Employee emp : list)
+		{
+			if(emp.getId() == 101)
+			{
+				return emp;
+			}
+		}
+		return null;
+	}
+	
+	
+	//----------------------------------------------------------------
+
+	@PostMapping("/save")
+	public String saveStudent() {
+		return "Student Saved";
+	}
+
+	@RequestMapping("/update")
+	public String updateStudent() {
+		return "Student Updated";
+	}
+	
+
+	@PostMapping("/delete")
+	public String deleteStudent() {
+		return "Student delete";
+	}
+	
 }
-
-
-
-
-
-
-
-
-
-
-
---
+++ Spring_Boot_1/src/main/java/com/qsp/Spring_Boot_1/Employee.java
@@ -0,0 +1,28 @@
+package com.qsp.Spring_Boot_1;
+
+public class Employee {
+	private int id;
+	private String name;
+	private long sal;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getSal() {
+		return sal;
+	}
+	public void setSal(long sal) {
+		this.sal = sal;
+	}
+	
+	
+
+}
