8bc6a8c79c8052ba079bb1703f6ba35ecd5a2daf
Rishabh Umredkar <umredkarrishabh@gmail.com>
2025-03-16 01:48:41 +0530

    Spring CRUD with delete by name

--- Trainer_CRUD/src/main/java/com/trainer/Trainer_CRUD/Controller.java
+++ Trainer_CRUD/src/main/java/com/trainer/Trainer_CRUD/Controller.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,54 +18,79 @@ public class Controller {
 
 	@Autowired
 	TrainerRepo repo;
-	
+
 	@PostMapping("/trainer")
-	public Trainer save(@RequestBody Trainer tr)
-	{
+	public Trainer save(@RequestBody Trainer tr) {
 		Trainer t = repo.save(tr);
 		return t;
 	}
-	
-	
+
 	@PutMapping("/trainer")
-	public Trainer updte(@RequestBody Trainer tr)
-	{
+	public Trainer updte(@RequestBody Trainer tr) {
 		Trainer t = repo.save(tr);
 		return t;
 	}
-	
+
 	@GetMapping("/trainer")
-	public List<Trainer> showAll()
-	{
+	public List<Trainer> showAll() {
 		List<Trainer> list = repo.findAll();
 		return list;
 	}
-	
-	
+
 	@GetMapping("trainer/{id}")
-	public Trainer findById(@PathVariable int id)
-	{
-		Optional<Trainer> optional =  repo.findById(id);
-		if(optional.isPresent())
-		{
+	public Trainer findById(@PathVariable int id) {
+		Optional<Trainer> optional = repo.findById(id);
+		if (optional.isPresent()) {
 			Trainer e = optional.get();
 			return e;
 		}
 		return null;
 	}
-	@GetMapping("trainer/findByName/{name}")
-	public List<Trainer> findByName(@PathVariable String name)
+
+	@DeleteMapping("trainer/{id}")
+	public void deleteByID(@PathVariable int id)
 	{
+		Optional<Trainer> optional = repo.findById(id);
+		if(optional.isPresent())
+		{
+			Trainer e = optional.get();
+			repo.delete(e);
+			
+		}
+	}
+	@DeleteMapping("trainer/deleteByName/{name}")
+	public int deleteByName(@PathVariable String name) {
+	    return repo.deleteByName(name);
+	}
+	
+	
+	
+	
+	
+	@GetMapping("trainer/findByName/{name}")
+	public List<Trainer> findByName(@PathVariable String name) {
 		return repo.findByName(name);
 	}
-	
+
 	@GetMapping("trainer/findBySub/{subject}")
-	public List<Trainer> findBySub(@PathVariable String subject)
-	{
+	public List<Trainer> findBySub(@PathVariable String subject) {
 		return repo.findBySubject(subject);
 	}
-	
-	
+
 
 	
+	@GetMapping("trainer/fetchByName/{name}")
+	public List<Trainer> FetchByName(@PathVariable String name)
+	{
+		return repo.fetchByName(name);
+	}
+
+	
+	@GetMapping("trainer/fetchBySub/{subject}")
+	public List<Trainer> FetchBySub(@PathVariable String subject)
+	{
+		return repo.FetchBySub(subject);
+	}
+	
+
 }
--- Trainer_CRUD/src/main/java/com/trainer/Trainer_CRUD/TrainerRepo.java
+++ Trainer_CRUD/src/main/java/com/trainer/Trainer_CRUD/TrainerRepo.java
@@ -3,6 +3,9 @@ package com.trainer.Trainer_CRUD;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import jakarta.transaction.Transactional;
 
 
 public interface TrainerRepo extends JpaRepository<Trainer, Integer>{
@@ -10,6 +13,17 @@ public interface TrainerRepo extends JpaRepository<Trainer, Integer>{
 	List<Trainer> findByName(String name);
 
 	List<Trainer> findBySubject(String subject);
+
+
+	@Query("select e from Trainer e where e.name = :name")
+	List<Trainer> fetchByName(String name);
+
+	@Query("select e from Trainer e where e.subject = :subject")
+	List<Trainer> FetchBySub(String subject);
+	
+	
+	@Transactional
+	int deleteByName(String name);
 
 
 }
